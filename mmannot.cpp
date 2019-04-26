315e69bf5a1440144231053d009e084de07cbc18
Matthias Zytnicki <matthias.zytnicki@toulouse.inra.fr>
2019-04-26 16:58:10 +0200

    Now MIT license.

--- mmannot.cpp
+++ mmannot.cpp
@@ -1,28 +1,11 @@
 /*
-Copyright (c) 2017, Matthias Zytnicki
-All rights reserved.
+Copyright 2019 Matthias Zytnicki
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the INRA nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL MATTHIAS ZYTNICKI BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 #include <iostream>
 #include <sstream>
@@ -501,7 +484,6 @@ bool rescue (vector < size_t > &regions) {
   size_t n = regions.size();
   if (n == 1) return false;
   size_t t = ceil(n * rescueThreshold);
-  size_t m = config->getNElements();
   vector < size_t > c(config->getNElements(), 0);
   for (size_t r: regions) {
     if (++c[r] >= t) {
@@ -586,8 +568,8 @@ class GtfLineParser {
 class XamRecord {
   protected:
     string name, chromosome;
-    unsigned int flags, nHits;
     Position start;
+    unsigned int flags, nHits;
     vector <pair <char, int>> cigar;
     size_t size;
     bool over;
@@ -1827,7 +1809,7 @@ class Counter {
         }
         sort(lines.begin(), lines.end());
         string currentName;
-        unsigned int count;
+        unsigned int count = 0;
         for (auto &line: lines) {
           if (line.first == currentName) {
             count += line.second;
